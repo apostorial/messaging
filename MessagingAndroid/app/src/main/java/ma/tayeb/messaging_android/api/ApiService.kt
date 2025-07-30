0804eb455bbeddb2eaf1feea1119c849ee3e5d4f
apostorial <t.elouadrassi@gmail.com>
2025-07-30 15:49:46 +0100

    damn

--- MessagingAndroid/app/src/main/java/ma/tayeb/messaging_android/api/ApiService.kt
+++ MessagingAndroid/app/src/main/java/ma/tayeb/messaging_android/api/ApiService.kt
@@ -1,11 +1,11 @@
 package ma.tayeb.messaging_android.api
 
+import ma.tayeb.messaging_android.enums.ReaderType
 import ma.tayeb.messaging_android.types.Customer
 import ma.tayeb.messaging_android.types.CustomerCreationRequest
-import ma.tayeb.messaging_android.types.Message
-import ma.tayeb.messaging_android.types.PaginatedResponse
+import retrofit2.Response
 import retrofit2.http.Body
-import retrofit2.http.GET
+import retrofit2.http.PATCH
 import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -15,10 +15,6 @@ interface ApiService {
     @POST("/api/customers/find-or-create")
     suspend fun findOrCreate(@Body request: CustomerCreationRequest): Customer
 
-    @GET("/api/messages/conversation/{conversationId}")
-    suspend fun getMessagesByConversationId(
-        @Path("conversationId") conversationId: UUID,
-        @Query("page") page: Int = 0,
-        @Query("size") size: Int = 10
-    ): PaginatedResponse<Message>
+    @PATCH("/api/messages/conversation/{conversationId}")
+    suspend fun markAsRead(@Path("conversationId") conversationId: UUID, @Query("readerType") readerType: ReaderType): Response<Void>
 }
--- Test/app/src/main/java/com/example/test/ApiService.kt
+++ Test/app/src/main/java/com/example/test/ApiService.kt
@@ -1,9 +1,16 @@
 package com.example.test
 
 import retrofit2.http.Body
+import retrofit2.http.PATCH
 import retrofit2.http.POST
+import retrofit2.http.Path
+import retrofit2.http.Query
+import java.util.UUID
 
 interface ApiService {
     @POST("/api/customers/find-or-create")
     suspend fun findOrCreate(@Body request: CustomerCreationRequest): Customer
+
+    @PATCH("/api/messages/conversation/{conversationId}")
+    suspend fun markAsRead(@Path("conversationId") conversationId: UUID, @Query("readerType") readerType: reader)
 }
